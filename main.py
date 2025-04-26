3ed8160edf3ab9e8515cbcc53739bc053f49403a
adrian <fbaltag83@gmail.com>
2025-04-26 17:38:52 -0400

    first commit / setup folder structure

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from refactored-mta!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/my_packages/docx_module/__init__.py
@@ -0,0 +1,3 @@
+"""`__init__.py`__
+This module initializes the 'docx' package.
+"""
