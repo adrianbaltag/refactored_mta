d8f31013e9435aab66f40805f04427e5c8ebedd6
adrianbaltag <fbaltag83@gmail.com>
2025-05-27 20:30:49 -0400

    completed mtas

--- src/my_packages/constants_module/urls.py
+++ src/my_packages/constants_module/urls.py
@@ -3,6 +3,6 @@
 """This module contains constants for URLs used in the project."""
 
 URL_FORMY = "https://formy-project.herokuapp.com/form"
-URL_REMEDY = "https://txslrempwp.vzwcorp.com/arsys/forms/rem1.vh.eng.vzwcorp.com/VZ+-+East+-+NRB/NRB/?cacheid=2378cbc6"
-URL_NSP = "https://nsp.vh.vzwnet.com/nsp/troubleshootingassistant"
-URL_MTAS = "https://mtas-web.vzwcorp.com/home.jsp"
+URL_REMEDY = 
+URL_NSP = 
+URL_MTAS = 
